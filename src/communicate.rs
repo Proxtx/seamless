8332f7c641eac5b600c7f1ba2d1420655c85cdad
Proxtx <gopro54122@gmail.com>
2023-10-26 13:27:54 +0200

    clean socket creation

--- src/communicate.rs
+++ src/communicate.rs
@@ -1,1 +1,59 @@
+use {
+    std::{
+        error::Error,
+        fmt,
+        net::{Ipv4Addr, SocketAddrV4},
+    },
+    tokio::net::UdpSocket,
+};
 
+type Result<T> = std::result::Result<T, CommunicateError>;
+
+#[derive(Debug)]
+pub enum CommunicateError {
+    SocketCreationError(std::io::Error),
+}
+
+impl fmt::Display for CommunicateError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            CommunicateError::SocketCreationError(ref err) => {
+                write!(f, "IO Error: {}", err.to_string())
+            }
+        }
+    }
+}
+
+impl Error for CommunicateError {}
+
+impl From<std::io::Error> for CommunicateError {
+    fn from(value: std::io::Error) -> Self {
+        CommunicateError::SocketCreationError(value)
+    }
+}
+
+pub struct Communicate {
+    send: UdpSocket,
+    receive: UdpSocket,
+    multicast_addr: SocketAddrV4,
+}
+
+impl Communicate {
+    pub async fn new(multicast_addr: SocketAddrV4, sender_port: u16) -> Result<Communicate> {
+        let receive_socket = UdpSocket::bind(SocketAddrV4::new(
+            Ipv4Addr::UNSPECIFIED,
+            multicast_addr.port(),
+        ))
+        .await?;
+        receive_socket.join_multicast_v4(*multicast_addr.ip(), Ipv4Addr::UNSPECIFIED)?;
+
+        let send_socket =
+            UdpSocket::bind(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, sender_port)).await?;
+
+        Ok(Communicate {
+            send: send_socket,
+            receive: receive_socket,
+            multicast_addr,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,15 @@
 use std::{net::SocketAddrV4, str::FromStr};
 
-use {
-    std::net::{Ipv4Addr, SocketAddr},
-    tokio::net::UdpSocket,
-};
+mod communicate;
 
-const GROUP_ID: &str = "225.0.0.1";
 const GROUP_ID_PORT: &str = "225.0.0.1:8000";
+const SENDER_PORT: u16 = 8001;
 
 #[tokio::main]
 async fn main() {
-    let t = std::sync::Arc::new(tokio::sync::Barrier::new(2));
-    let t2 = t.clone();
-    tokio::spawn(async move {
-        receiver().await;
-        t.wait().await;
-    });
-
-    sender().await;
-
-    t2.wait().await;
-}
-
-async fn sender() {
-    let socket = UdpSocket::bind("0.0.0.0:8001").await.unwrap();
-    socket
-        .send_to(
-            b"Hello whats up",
-            SocketAddrV4::from_str(GROUP_ID_PORT).unwrap(),
-        )
+    let group_address: SocketAddrV4 =
+        SocketAddrV4::from_str(GROUP_ID_PORT).expect("Invalid Group ID or Port");
+    let communicate = communicate::Communicate::new(group_address, SENDER_PORT)
         .await
         .unwrap();
-}
-
-async fn receiver() {
-    let socket = UdpSocket::bind("0.0.0.0:8000").await.unwrap();
-    socket
-        .join_multicast_v4(Ipv4Addr::from_str(GROUP_ID).unwrap(), Ipv4Addr::UNSPECIFIED)
-        .unwrap();
-
-    let mut buf = [0; 2024];
-    loop {
-        let (amt, src) = socket.recv_from(&mut buf).await.unwrap();
-
-        let buf = &mut buf[..amt];
-
-        println!("received: {}", std::str::from_utf8(buf).unwrap());
-    }
 }
