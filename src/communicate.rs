76d7e49d089cc067a5b42169656f49d33ccd0510
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-12-06 22:10:10 +0100

    remove too much log

--- src/communicate.rs
+++ src/communicate.rs
@@ -161,10 +161,7 @@ impl Communicate {
                 Ok((amount, socket_addr)) => {
                     let buf = &mut buf[..amount];
                     match std::str::from_utf8(buf) {
-                        Ok(msg) => {
-                            println!("{}", msg);
-                            callback(msg, socket_addr)
-                        }
+                        Ok(msg) => callback(msg, socket_addr),
                         Err(e) => {
                             println!("Error converting buffer to String: {}", e)
                         }
